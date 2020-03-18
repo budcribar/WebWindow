3ecf04de3580a4fd5d839d547d747e6b136ce705
Bud Cribar <budcribar@msn.com>
2020-03-18 06:24:58 -0600

    Clean up interface

--- src/WebWindow/IWebWindow.cs
+++ src/WebWindow/IWebWindow.cs
@@ -5,13 +5,12 @@ using System.Text;
 namespace WebWindows
 {
     public interface IWebWindow
-    {
-        void WaitForExit();
+    {    
+        void SendMessage(string message);
         void Show();
         void ShowMessage(string title, string body);
-        void Invoke(Action workItem);
         void NavigateToUrl(string url);
+        void WaitForExit();
         event EventHandler<string> OnWebMessageReceived;
-        void SendMessage(string message);
     }
 }
