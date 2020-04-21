bf82b49bd5abd66942c5a0cdbc6044c379e6a640
Bud Cribar <budcribar@msn.com>
2020-04-21 11:15:05 -0600

    Working locally

--- src/RemoteableWebWindowService/Program.cs
+++ src/RemoteableWebWindowService/Program.cs
@@ -53,8 +53,9 @@ namespace PeakSwc.RemoteableWebWindows
                     webBuilder.ConfigureKestrel(options => {
                         //options.Listen(IPAddress.Loopback, 80);
                         //options.Listen(IPAddress.Parse( "13.64.108.0"), 443, listenOptions => { listenOptions.UseHttps(); });
-                        options.Listen(IPAddress.Parse("10.1.0.4"), 443, lo => { lo.UseHttps("a419da49-1b24-460a-8397-6be2d80c41f2.pfx", ""); });
-                        //options.Listen(IPAddress.Loopback, 443, listenOptions => { listenOptions.UseHttps(); });
+                        
+                        //options.Listen(IPAddress.Parse("10.1.0.4"), 443, lo => { lo.UseHttps("a419da49-1b24-460a-8397-6be2d80c41f2.pfx", ""); });
+                        options.Listen(IPAddress.Loopback, 443, listenOptions => { listenOptions.UseHttps(); });
                     });
                     webBuilder.UseStartup<Startup>();
                 });
