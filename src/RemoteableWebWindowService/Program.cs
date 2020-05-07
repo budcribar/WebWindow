bc4df7d8ecb86d8f5cee4b463da6b5a92131369c
Bud Cribar <budcribar@msn.com>
2020-05-07 11:34:27 -0600

    local

--- src/RemoteableWebWindowService/Program.cs
+++ src/RemoteableWebWindowService/Program.cs
@@ -55,8 +55,8 @@ namespace PeakSwc.RemoteableWebWindows
                         //options.Listen(IPAddress.Parse( "13.64.108.0"), 443, listenOptions => { listenOptions.UseHttps(); });
 
                         //options.Listen(IPAddress.Parse("10.1.0.4"), 443, lo => { lo.UseHttps("a419da49-1b24-460a-8397-6be2d80c41f2.pfx", ""); });
-                        options.Listen(IPAddress.Parse("18.217.178.146"), 80, lo => { lo.UseHttps("poc_certificate.pfx", "boldtek@2020"); });
-                        // options.Listen(IPAddress.Loopback, 443, listenOptions => { listenOptions.UseHttps(); });
+                        //options.Listen(IPAddress.Parse("18.217.178.146"), 80, lo => { lo.UseHttps("poc_certificate.pfx", "boldtek@2020"); });
+                        options.Listen(IPAddress.Loopback, 443, listenOptions => { listenOptions.UseHttps(); });
                     });
                     webBuilder.UseStartup<Startup>();
                 });
